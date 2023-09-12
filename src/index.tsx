4ea0f12968e389a2f355ebe78747fa7877aae52c
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-12 22:36:24 +0200

    wait until server is listening before logging

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import { html } from '@elysiajs/html';
 import { Elysia } from 'elysia';
 
-const app = new Elysia()
-  .use(html())
-  .get('/', () => <h1>Hello, World!</h1>)
-  .listen(3000);
+const app = new Elysia().use(html()).get('/', () => <h1>Hello, World!</h1>);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.listen(3000, ({ hostname, port }) => {
+  console.log(`🦊 Elysia is running at http://${hostname}:${port}`);
+});
