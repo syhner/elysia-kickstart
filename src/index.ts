052f395b7c0594195a0b6f40a6183f25a464ed07
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-12 13:49:48 +0200

    add elysia

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia';
+
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
