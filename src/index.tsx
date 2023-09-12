611979c18cd37bd200b505043b48608f064de388
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-12 19:34:55 +0200

    jsx support

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { html } from '@elysiajs/html';
+import { Elysia } from 'elysia';
+
+const app = new Elysia()
+  .use(html())
+  .get('/', () => <h1>Hello, World!</h1>)
+  .listen(3000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
