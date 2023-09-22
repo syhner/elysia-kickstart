17c1619d21585f52795a5db608632c9e547098ab
Siraj Hussain <71605633+Syhner@users.noreply.github.com>
2023-09-22 21:34:58 +0200

    fix formatting

--- api/index.tsx
+++ api/index.tsx
@@ -3,9 +3,11 @@
  */
 
 import '../src/env'; // Validate env vars
+
 import { app } from '../src/app';
 
 export const config = { runtime: 'edge' };
+
 export default async function handler(request: Request) {
   return app.fetch(request);
 }
